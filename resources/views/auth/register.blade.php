adcc037894f9da460220d230c0dd567a7c8dc805
reqayasa <reqayasateknik@gmail.com>
2023-12-05 16:49:49 +0700

    Add frontend template
    Integrasi template html BLOGY untuk Login, Register, dan Index

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="comment-form-wrap pt-5">
+        <h3 class="mb-5">{{ __('Register') }}</h3>
+        <form method="POST" action="{{ route('register') }}" class="p-5 bg-light">
+
+            @csrf
+            
+            <div class="form-group">
+                <label for="name">{{ __('Name') }} *</label>
+                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror            </div>
+            <div class="form-group">
+                <label for="email">{{ __('Email Address') }} *</label>
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="password">{{ __('Password') }} *</label>
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="password-confirm">{{ __('Confirm Password') }} *</label>
+                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+            </div>
+
+            <div class="form-group">
+                <button type="submit"  class="btn btn-primary">{{ __('Register') }}</button>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/posts/index', [App\Http\Controllers\Post\PostController::class, 'index'])->name('posts.index');
